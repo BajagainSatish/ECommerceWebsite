99909c020a6fa9717dd7e7c22d4015d6520eb499
Satish Bajagain <sttbjgn@gmail.com>
2025-01-25 13:31:20 +0545

    small changes

---
+++ root_ecommerce_website/frontend/UI_UX/ecommerce_uiux_finalproj_satish/src/components/productsData.ts
@@ -0,0 +1,27 @@
+// productsData.ts
+export const products = [
+  {
+    id: 1,
+    name: "Fjallraven Backpack",
+    price: 109.95,
+    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    description: "Your perfect pack for everyday use and walks in the forest.",
+  },
+  {
+    id: 2,
+    name: "Mens Casual T-Shirt",
+    price: 22.3,
+    image:
+      "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+    description:
+      "Slim-fitting style, contrast raglan long sleeve, three-button henley placket.",
+  },
+  {
+    id: 3,
+    name: "Mens Cotton Jacket",
+    price: 55.99,
+    image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+    description: "Great outerwear jackets for Spring/Autumn/Winter.",
+  },
+  // Add more products here as needed
+];
