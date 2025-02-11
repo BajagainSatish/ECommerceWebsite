6cf9c3f3a52d40dd372d85d1ad4a8e88b352d23f
Satish Bajagain <sttbjgn@gmail.com>
2025-02-11 23:01:32 +0545

    Actual success of brand addition from cms to database.

--- root_ecommerce_website/backend/dotnet/EcommerceAPI/obj/Debug/net9.0/EcommerceAPI.AssemblyInfo.cs
+++ root_ecommerce_website/backend/dotnet/EcommerceAPI/obj/Debug/net9.0/EcommerceAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc62936e257820689328de22724535b5ee6891f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23b2677b391dd841867a61e162b936b2bc3687d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
